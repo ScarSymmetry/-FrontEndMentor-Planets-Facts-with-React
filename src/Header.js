02659c36292124b247af78ff8d339563d4ce9731
ScarSymmetry <caparisonhgs@gmail.com>
2021-09-19 08:04:53 +0300

    mobile menu circles

--- src/Header.js
+++ src/Header.js
@@ -1,14 +1,18 @@
 import Nav from "./Nav";
-import styles from "./Header.module.css"
+import styles from "./Header.module.css";
+import burger from "./svg/icon-hamburger.svg";
 const Header = ({ data, changePlanet, activePlanet }) => {
 	return (
-		<div className={styles.navbar}>
-			<h1 className={styles.navbar__title}>The Planets</h1>
-			<Nav
-				changePlanet={changePlanet}
-				data={data}
-				activePlanet={activePlanet}
-			/>
+		<div className="wrapper">
+			<div className={styles.navbar}>
+				<h1 className={styles.navbar__title}>The Planets</h1>
+				<Nav
+					changePlanet={changePlanet}
+					data={data}
+					activePlanet={activePlanet}
+				/>
+				<img className={styles.burger} src={burger} alt="menu" />
+			</div>
 		</div>
 	);
 };
